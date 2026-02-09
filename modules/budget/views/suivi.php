5eb1aec8ff8054f24aec9e723dc1bc24879de9fc
fefe.clochette <ferlan.alexandre@gmail.com>
2026-02-09 17:53:11 +0100

    Auto-commit for deploy to production - 2026-02-09 17:53:11

--- modules/budget/views/suivi.php
+++ modules/budget/views/suivi.php
@@ -1,6 +1,10 @@
 <?php
 // modules/budget/views/suivi.php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // ============================================================================
 // 1. RÉCUPÉRATION DES BUDGETS PRÉVISIONNELS (Dynamique)
 // ============================================================================
